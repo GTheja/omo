544aee14887132410856ebdf7d23b8de5bd6b8ed
Theja <theja619@gmail.com>
2020-06-30 19:01:06 +0530

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC3WGLvq4m3U9mhFvhiS5TjGybawAbUYCs",
+    authDomain: "sosoci-2bec7.firebaseapp.com",
+    databaseURL: "https://sosoci-2bec7.firebaseio.com",
+    projectId: "sosoci-2bec7",
+    storageBucket: "sosoci-2bec7.appspot.com",
+    messagingSenderId: "229304922552",
+    appId: "1:229304922552:web:92e5b806ef58586eaf5f4d",
+    measurementId: "G-8P6X4V9TRS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
